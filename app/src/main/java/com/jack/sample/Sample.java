b6e9009f22c86ed6060cc3d27d1b26daaa55dd38
TSL124 <tsl124@TSL124s-MacBook-Air.local>
2019-08-26 10:44:14 -0400

    First commit

---
+++ app/src/main/java/com/jack/sample/Sample.java
@@ -0,0 +1,17 @@
+package com.jack.sample;
+
+public class Sample {
+    private String sample;
+
+    public Sample(String sample) {
+        this.sample = sample;
+    }
+
+    public String getSample() {
+        return sample;
+    }
+
+    public void setSample(String sample) {
+        this.sample = sample;
+    }
+}
